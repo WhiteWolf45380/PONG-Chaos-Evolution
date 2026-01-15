ee2fc6fa9046b403de950fa6e3ecc042ae245a5a
WhiteWolf <matheodeponte45@gmail.com>
2026-01-16 00:09:55 +0100

    pm working

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ import pygame_manager as pm
 
 class Main:
     def __init__(self):
-        pass
+        pm.init()
+        self.closed = False
+        self.opened = False
 
     def update(self):
-        pass
+        print(pygame.time.get_ticks())
+        if pygame.time.get_ticks() >= 5000 and not self.closed:
+            pm.screen.close()
+            self.closed = True
+        if pygame.time.get_ticks() >= 10000 and not self.opened:
+            pm.screen.create()
+            self.opened = True
 
 if __name__ == '__main__':
     main = Main()
-    pm.run(main.update())
+    pm.run(main.update)
