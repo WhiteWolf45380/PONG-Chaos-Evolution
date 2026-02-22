ab2144371a067961e2e5032377e0839b32d6abed
WhiteWolf <matheodeponte45@gmail.com>
2026-02-22 14:49:16 +0100

    working on: settings

--- pong/_menus/settings/_panels/view.py
+++ pong/_menus/settings/_panels/view.py
@@ -6,11 +6,25 @@ class SettingsMenuView(pm.panels.Panel):
     """
     Panel de vue du menu des paramètres
     """
-    def __init__(self, width : int = 1920, height : int = 1080):
+    def __init__(self, width : int = 1440, height : int = 1080):
         super().__init__('settings_menu_view', rect=(0, 0, width, height), centered=True)
 
         # Fond
         self.background_color = (0, 0, 15)
+
+        # Titre
+        self.title = pm.ui.Text(
+            x=self.centerx,
+            y=self.height * 0.04,
+            anchor="midtop",
+            text=pm.languages("settings_title"),
+            font="davidclmmedium",
+            font_size=96,
+            font_color=(210, 225, 230),
+            shadow=True,
+            underline=True,
+            panel=str(self),
+        )
 
     def draw_back(self, surface: pygame.Surface):
         """Dessin par frame"""
--- pong/_menus/settings/state.py
+++ pong/_menus/settings/state.py
@@ -16,7 +16,20 @@ class Settings(pm.states.State):
         self.bind_panel(self.view)
 
         # Paramètres
-        pm.settings.create("p1_up", pygame.K_z)
-        pm.settings.create("p1_down", pygame.K_s)
-        pm.settings.create("p2_up", pygame.K_UP)
-        pm.settings.create("p2_down", pygame.K_DOWN)
+        pm.settings.create("p1_up", pygame.K_z, category="inputs", widget="inputbutton")
+        pm.settings.create("p1_down", pygame.K_s, category="inputs", widget="inputbutton")
+        pm.settings.create("p2_up", pygame.K_UP, category="inputs", widget="inputbutton")
+        pm.settings.create("p2_down", pygame.K_DOWN, category="inputs", widget="inputbutton")
+
+        # Panel déroulant des paramètres
+        self.parameters = pm.settings.Panel(
+            name="settings_menu_parameters",
+            predecessor=str(self.view),
+            x=self.view.centerx,
+            y=self.view.centery,
+            width=1080,
+            height=700,
+            centered=True,
+        )
+        self.bind_panel(self.parameters.panel)
+        self.parameters.activate()
